3220c904d763346f85a8d0a96ddbbfa5c99db721
dylan <dcjohnson24@gmail.com>
2020-07-15 18:45:27 -0500

    Make changes to landing page; add endpoint for knn

--- api/api.py
+++ api/api.py
@@ -1,8 +1,18 @@
 import time
-from flask import Flask
+import sys
+import os
+sys.path.append(os.pardir)
+
+from flask import Flask, request
+
+from model.train import main
 
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+
+@app.route('/api/dogs')
+def get_nearest_neighbors():
+    dog_breed = request.args.get('dog_breed')
+    n_neighbors = request.args.get('n_neighbors')
+    neighbors = main(dog_breed, n_neighbors)
+    return {'neighbors': neighbors}
--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
+import logo from './dog_tree.jpg';
 import './App.css';
 
+
 function App() {
+  let nearest_neighbors = ['Poodles', 'Dalmations', 'Golden Retrievers'];
+  const display = nearest_neighbors.map((number) => 
+    <li>{number}</li>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div classname="App">
+      <header classname="App-header">
+        <img src={logo} alt="logo" />
+        <h1>Welcome to the Dog KNN App!</h1>
+        <h2>Find the nearest neighbors of your favorite dogs</h2>
       </header>
+      <Input/>
+      <p> The nearest neighbors of Rotweiler are 
+        <ul>
+          {display}
+        </ul>
+      </p>
     </div>
+    
   );
 }
+
+function Input() {
+  return (
+    <div>
+      Dogs: <input type="text" classname="Search" placeholder="Search..." /><br></br><br></br>
+      Number of Neighbors: <input type="text" classname="input" placeholder="Number of neighbors e.g. 3..." />
+      <p>
+        <input type="submit"></input>
+      </p>
+    </div>
+
+  );
+}
+
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+)
 
 export default App;
